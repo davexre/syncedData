a600abe75f0870019ff59449bf60642f24c5136a
davexre <63078133+davexre@users.noreply.github.com>
2021-01-13 15:31:19 -0600

    More updates to comments

--- syncedData.go
+++ syncedData.go
@@ -1,21 +1,5 @@
-/*
-syncedData implements simple data types that are protected for concurrent use. Currently, the
-package only contains a simple Counter, but will be extended in the future. Use:
-
-	include "fmt"
-	include "github.com/davexre/syncedData"
-
-	func main() {
-		var myCounter syncedData.Counter
-
-		myCounter.Incr()
-		myCounter.Incr()
-		myCounter.Decr()
-
-		fmt.Println("Who's number %v?", myCounter.Read())
-	}
-
-*/
+// syncedData implements simple data types that are protected for concurrent use. Currently, the
+// package only contains a simple Counter, but will be extended in the future.
 package syncedData
 
 import (
@@ -24,6 +8,20 @@ import (
 
 // Counter provides a simple counting interface that's made thread safe through
 // use of a Mutex. Several methods are implemented to do typical functions.
+// Use:
+//
+// 	include "fmt"
+// 	include "github.com/davexre/syncedData"
+//
+// 	func main() {
+// 		var myCounter syncedData.Counter
+//
+// 		myCounter.Incr()
+// 		myCounter.Incr()
+// 		myCounter.Decr()
+//
+// 		fmt.Println("Who's number %v?", myCounter.Read())
+// 	}
 type Counter struct {
 	m     sync.Mutex
 	count int
