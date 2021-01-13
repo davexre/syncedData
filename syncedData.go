2e4d8da9af2d934589dd66ea182877ea781b5276
davexre <63078133+davexre@users.noreply.github.com>
2021-01-13 14:49:49 -0600

    Added documentation and the Decr() method

--- syncedData.go
+++ syncedData.go
@@ -1,17 +1,30 @@
+/*
+syncedData implements simple data types that are protected for concurrent use.
+*/
 package syncedData
 
 import (
 	"sync"
 )
 
+// Counter provides a simple counting interface that's made thread safe through
+// use of a Mutex. Several methods are implemented to do typical functions.
+// Incr() adds 1 to the counter. Decr() subtracts 1. Read() returns the
+// current value of the Counter.
 type Counter struct {
-	m			sync.Mutex
-	count	int
+	m     sync.Mutex
+	count int
 }
 
 func (c *Counter) Incr() {
 	c.m.Lock()
 	c.count++
+	c.m.Unlock()
+}
+
+func (c *Counter) Decr() {
+	c.m.Lock()
+	c.count--
 	c.m.Unlock()
 }
 
